6c8c79a1ec4324ae0bf509a664a9714f553f2f1a
alexpin <intracomof@gmail.com>
2021-10-17 17:15:06 +0700

    init

---
+++ src/Controller.php
@@ -0,0 +1,15 @@
+<?php
+namespace Allnet;
+
+class Controller
+{
+    public function index()
+    {
+        return 'https://allnet.me';
+    }
+
+    public function sanaplius()
+    {
+        return 'https://allnet.me/internet/odessa/sana-plius/';
+    }
+}
